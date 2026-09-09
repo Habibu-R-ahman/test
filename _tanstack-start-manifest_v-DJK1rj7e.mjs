983da76aae56fab107df152a1b0bb095e6264cd5
b1ackc0d3 <35610644+Habibu-R-ahman@users.noreply.github.com>
2026-09-09 23:56:03 +0600

    Add files via upload

---
+++ _tanstack-start-manifest_v-DJK1rj7e.mjs
@@ -0,0 +1,20 @@
+//#region node_modules/.nitro/vite/services/ssr/assets/_tanstack-start-manifest_v-DJK1rj7e.js
+var tsrStartManifest = () => ({ routes: {
+	__root__: {
+		filePath: "/workspace/src/routes/__root.tsx",
+		children: ["/"],
+		preloads: ["/assets/index-DHJGvq8i.js"],
+		scripts: [{ attrs: {
+			type: "module",
+			async: !0,
+			src: "/assets/index-DHJGvq8i.js"
+		} }]
+	},
+	"/": {
+		filePath: "/workspace/src/routes/index.tsx",
+		children: void 0,
+		preloads: ["/assets/routes-NaBUW9ir.js"]
+	}
+} });
+//#endregion
+export { tsrStartManifest };
